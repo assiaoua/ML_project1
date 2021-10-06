9a118afb5b4ae24fdf8c058d5f09501c6ae9f6cb
ch-epfl-287707 <ch-epfl-287707@noto.epfl.ch>
2021-10-06 16:22:26 +0200

    creation main et helpers functions

---
+++ functions.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""some helper functions."""
+import numpy as np
+
+def least_squares_GD(y, tx, initial w, max_iters, gamma):
+    
+
+    return w, loss
+
+def least_squares_SGD(y, tx, initial w, max_iters, gamma):
+    
+    return w, loss
+
+def least_squares(y, tx) :
+    
+    return w, loss
+
+def ridge_regression(y, tx, lambda_) :
+    
+    return w, loss
+
+def logistic_regression(y, tx, initial w, max_iters, gamma) :
+    
+    return w, loss
+
+def reg_logistic_regression(y, tx, lambda_ , initial w, max_iters, gamma) :
+    
+    return w, loss
+
+
